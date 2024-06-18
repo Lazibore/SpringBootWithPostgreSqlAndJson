6c1f21bde9ed77f10d55ff6b8f20c75e72a7cb54
Lazibore <37276578+Lazibore@users.noreply.github.com>
2024-06-18 08:20:02 +0300

    First

---
+++ PostgreSqlConnectionDemo/src/main/java/com/jdbcPostgreSqlConnection/PostgreSqlConnectionDemo/PostgreSqlConnectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jdbcPostgreSqlConnection.PostgreSqlConnectionDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostgreSqlConnectionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostgreSqlConnectionDemoApplication.class, args);
+	}
+
+}
---
+++ PostgreSqlConnectionDemo/src/test/java/com/jdbcPostgreSqlConnection/PostgreSqlConnectionDemo/PostgreSqlConnectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdbcPostgreSqlConnection.PostgreSqlConnectionDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostgreSqlConnectionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
